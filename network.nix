bf124b05107f2ded7d100922528a38fb7ce0730f
Vladislav Trofimenko <foss@slashfast.dev>
2024-07-23 11:53:04 +0300

    update

---
+++ network.nix
@@ -0,0 +1,15 @@
+{}:{
+  networking.useDHCP = false;
+
+  systemd.network.networks."10-enp3s0" = {
+    matchConfig.Name = "enp3s0";
+    networkConfig = {
+      # start a DHCP Client for IPv4 Addressing/Routing
+      DHCP = "ipv4";
+      # accept Router Advertisements for Stateless IPv6 Autoconfiguraton (SLAAC)
+      IPv6AcceptRA = true;
+    };
+    # make routing on this interface a dependency for network-online.target
+    linkConfig.RequiredForOnline = "routable";
+  };
+}
