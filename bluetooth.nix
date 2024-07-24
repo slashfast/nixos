7dd4f1da5769b056e8570decee053f0d1ae10bb2
Vladislav Trofimenko <foss@slashfast.dev>
2024-07-25 00:33:42 +0300

    update

--- bluetooth.nix
+++ bluetooth.nix
@@ -11,18 +11,18 @@
       General = {
         Name = "slashfast-pc";
         ControllerMode = "bredr";       
-        MultiProfile = "off";
-        Experimental = true;
+        #MultiProfile = "off";
+        #Experimental = true;
       };
     };
-    disabledPlugins = ["bap" "bass" "mcp" "vcp" "micp" "ccp" "csip"];
+    #disabledPlugins = ["bap" "bass" "mcp" "vcp" "micp" "ccp" "csip"];
   };
   #services.blueman.enable = true;
 
-  systemd.user.services.mpris-proxy = {
-    description = "Mpris proxy";
-    after = ["network.target" "sound.target"];
-    wantedBy = ["default.target"];
-    serviceConfig.ExecStart = "${pkgs.bluez}/bin/mpris-proxy";
-  };
+  #systemd.user.services.mpris-proxy = {
+  #  description = "Mpris proxy";
+  #  after = ["network.target" "sound.target"];
+  #  wantedBy = ["default.target"];
+  #  serviceConfig.ExecStart = "${pkgs.bluez}/bin/mpris-proxy";
+  #};
 }
